57f39b2f15ac1f5d5121a113607b3bfecfa45388
sridevi77548 <82494177+sridevi77548@users.noreply.github.com>
2026-01-23 14:29:13 +0530

    /Hello-World in AWS

---
+++ hello-world.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handleRequest() (string, error) {
+	return "Hello from AWS lambda function", nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
